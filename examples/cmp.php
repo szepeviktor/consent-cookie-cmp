2f7b11c61d86e15bf34b3795852815753392233f
Viktor Szépe <viktor@szepe.net>
2026-03-29 14:05:04 +0000

    Update example CMP embeds

--- examples/cmp.php
+++ examples/cmp.php
@@ -3,21 +3,20 @@
 /*
  * Plugin Name: Consent Cookie CMP Example
  * Description: Minimal example plugin that injects the standalone CMP assets with wp_head and wp_footer hooks.
- * Version: 0.1.0
  */
 
-declare(strict_types=1);
-
-add_action('wp_head', static function (): void {
+add_action('wp_head', static function () {
     ?>
+    <!-- Consent Cookie CMP -->
     <link rel="stylesheet" href="/cmp/klaro.css">
     <link rel="stylesheet" href="/cmp/cmp.css">
     <script src="/cmp/cmp-bootstrap.js" data-gtm-id="GTM-XXXXXXX"></script>
     <?php
 });
 
-add_action('wp_footer', static function (): void {
+add_action('wp_footer', static function () {
     ?>
+    <!-- Consent Cookie CMP -->
     <script src="/cmp/klaro-config.js"></script>
     <script src="/cmp/klaro.js" defer></script>
     <?php
